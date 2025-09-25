4751821c722560c6dc0ec3b5f1731384bd0dcaf9
kaziabulhasib <hasibkazi420@gmail.com>
2025-09-26 02:00:34 +0530

    left sidebar design complete

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,7 +1,122 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
 
 const LeftBar = () => {
-  return <div>LeftBar</div>;
+  return (
+    <div className=' h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8'>
+      {/* LOGO MENU BUTTONS  */}
+      <div className=' flex flex-col gap-4 items-center xxl:items-start'>
+        {/* LOGO  */}
+        <Link className='p-2 rounded-full bg-[#181818] ' href='/'>
+          <Image src='icons/logo.svg' alt='logo' width={24} height={24} />
+        </Link>
+        {/* MENU LIST  */}
+        <div className='flex flex-col gap-4'>
+          {menuList.map((item) => (
+            <Link
+              className='p-2 rounded-full bg-[#181818] flex items-center gap-4'
+              href={item.link}
+              key={item.id}>
+              <Image
+                src={`icons/${item.icon}`}
+                alt={item.name}
+                height={24}
+                width={24}
+              />
+              <span className='hidden xxl:inline'>{item.name}</span>
+            </Link>
+          ))}
+        </div>
+        {/* BUTTON  */}
+        <Link
+          className=' bg-white text-black font-bold rounded-full w-12  h-12 flex items-center justify-center xxl:hidden'
+          href='/'>
+          <Image src='icons/post.svg' alt='new post' width={24} height={24} />
+        </Link>
+        <Link
+          href='/'
+          className='hidden xxl:block bg-white text-black font-bold rounded-full py-2 px-20'>
+          Post
+        </Link>
+      </div>
+      {/* USER  */}
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-2 '>
+          <div className='w-10 h-10 relative rounded-full overflow-hidden'>
+            <Image src='/general/avatar.png' alt='user name' fill />
+          </div>
+          <div className='hidden xxl:flex flex-col'>
+            <span className='font-bold'>username</span>
+            <span className='text-sm text-textGray'>handle@handle.com</span>
+          </div>
+        </div>
+        <div className='hidden xxl:block cursor-pointer font-bold'>...</div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftBar;
