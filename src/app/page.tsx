90fc6df0301f38fef5df7013426eaf5c3b0388dd
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-01 22:53:00 +0530

    create :feed section - first parte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
+import Feed from "@/components/Feed";
+import Share from "@/components/Share";
+import Link from "next/link";
+
 const Homepage = () => {
-  return <div className='relative h-[600px] w-[600px]'>homePage</div>;
+  return (
+    <div className='relative h-[600px] w-[600px]'>
+      <div className=' px-4 pt-4 flex items-center justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
+        <Link
+          className='pb-3 flex items-center border-b-4 border-iconBlue'
+          href='/'>
+          For You
+        </Link>
+        <Link className='pb-3 flex items-center ' href='/'>
+          Following
+        </Link>
+        <Link className='pb-3 flex items-center ' href='/'>
+          css
+        </Link>
+        <Link className='pb-3 flex items-center ' href='/'>
+          Javascript
+        </Link>
+        <Link className='pb-3 flex items-center ' href='/'>
+          React
+        </Link>
+      </div>
+      <Share />
+      <Feed />
+    </div>
+  );
 };
 
 export default Homepage;
