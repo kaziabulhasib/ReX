4209c78dc66a22ec21074ac3e75beb98079640ed
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-02 13:19:22 +0530

    server action upload file using imagekit

--- src/actions.tsx
+++ src/actions.tsx
@@ -1,9 +1,32 @@
 "use server";
 
-export const shareActions= async (formData:FormData)=>{
-    const file= formData.get("file") as File;
-    const desc= formData.get("desc") as string;
+import ImageKit from "imagekit";
 
-    console.log(file, desc);
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+});
 
-}
+export const shareActions = async (formData: FormData) => {
+  const file = formData.get("file") as File;
+  //   const desc = formData.get("desc") as string;
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  imagekit.upload(
+    {
+      file: buffer,
+      fileName: file.name,
+      folder: "/ReX-app/posts",
+      transformation: {
+        pre: "w-600",
+      },
+    },
+    function (error, result) {
+      if (error) console.log(error);
+      else console.log(result);
+    }
+  );
+};
