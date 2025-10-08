e798fb562b21deaba7aee0e15ce9e6bd634d360e
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-08 11:30:44 +0530

    fetch data on server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Feed from "@/components/Feed";
 import Share from "@/components/Share";
-import { prisma } from "@/prisma";
+// import { prisma } from "@/prisma";
 import Link from "next/link";
 
-const Homepage =  () => {
-
+const Homepage = () => {
   return (
     <div className='relative h-[600px] w-[600px]'>
       <div className=' px-4 pt-4 flex items-center justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Post from "./Post";
+import { prisma } from "@/prisma";
 
-const Feed = () => {
-  return <div>
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-  </div>;
+const Feed = async () => {
+  const posts = await prisma.post.findMany();
+  return (
+    <div>
+      {posts.map((post) => (
+        <div className='' key={post.id}>
+          <Post type="post" />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Feed;
