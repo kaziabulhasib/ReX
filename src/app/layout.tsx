dd2d94310d43e834551387e3b738a1688ec59d70
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-08 12:59:17 +0530

    create auth setup with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import RightBar from "@/components/RightBar";
 
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Rex Social",
@@ -18,21 +19,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body>
-        <div className=' max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex '>
-          <div className='px-2 xsm:px-4 xxl:px-8  '>
-            <LeftBar />
+      <ClerkProvider>
+        <body>
+          <div className=' max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex '>
+            <div className='px-2 xsm:px-4 xxl:px-8  '>
+              <LeftBar />
+            </div>
+            <div className='flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray h-screen hide-scrollbar overflow-y-auto '>
+              {children}
+              {modal}
+            </div>
+            <div className='hidden lg:flex ml-4 md:ml-8 flex-1 '>
+              <RightBar />
+            </div>
           </div>
-          <div className='flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray h-screen hide-scrollbar overflow-y-auto '>
-            {children}
-            {modal}
-          </div>
-          <div className='hidden lg:flex ml-4 md:ml-8 flex-1 '>
-            <RightBar />
-          </div>
-        </div>
-        {/* with grid  */}
-        {/* <div className='max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto grid grid-cols-[auto_1fr] lg:grid-cols-[auto_1fr_auto] gap-0'>
+          {/* with grid  */}
+          {/* <div className='max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto grid grid-cols-[auto_1fr] lg:grid-cols-[auto_1fr_auto] gap-0'>
           <div className='px-2 xsm:px-4 xxl:px-8'>
             <LeftBar />
           </div>
@@ -44,8 +46,9 @@ export default function RootLayout({
             <RightBar />
           </div>
         </div> */}
-        {/* with grid end  */}
-      </body>
+          {/* with grid end  */}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
+
+const SignUPPage = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignUPPage;
