c60feb74cf57e0f2f5aa66e12030595aabd5040b
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-09 10:21:45 +0530

    restructure the layouts

---
+++ src/app/(board)/layout.tsx
@@ -0,0 +1,33 @@
+import LeftBar from "@/components/LeftBar";
+import "./globals.css";
+import RightBar from "@/components/RightBar";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rex Social",
+  description: "Next.js social media application project",
+};
+
+export default function BoardLayout({
+  children,
+  modal,
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
+  return (
+    <div className=' max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex '>
+      <div className='px-2 xsm:px-4 xxl:px-8  '>
+        <LeftBar />
+      </div>
+      <div className='flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray h-screen hide-scrollbar overflow-y-auto '>
+        {children}
+        {modal}
+      </div>
+      <div className='hidden lg:flex ml-4 md:ml-8 flex-1 '>
+        <RightBar />
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,8 @@ export const metadata: Metadata = {
   description: "Next.js social media application project",
 };
 
-export default function RootLayout({
+export default function BoardLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
   modal: React.ReactNode;
@@ -20,34 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <ClerkProvider>
-        <body>
-          <div className=' max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex '>
-            <div className='px-2 xsm:px-4 xxl:px-8  '>
-              <LeftBar />
-            </div>
-            <div className='flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray h-screen hide-scrollbar overflow-y-auto '>
-              {children}
-              {modal}
-            </div>
-            <div className='hidden lg:flex ml-4 md:ml-8 flex-1 '>
-              <RightBar />
-            </div>
-          </div>
-          {/* with grid  */}
-          {/* <div className='max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto grid grid-cols-[auto_1fr] lg:grid-cols-[auto_1fr_auto] gap-0'>
-          <div className='px-2 xsm:px-4 xxl:px-8'>
-            <LeftBar />
-          </div>
-          <div className='lg:min-w-[600px] border-x-[1px] border-borderGray overflow-y-auto'>
-            {children}
-            {modal}
-          </div>
-          <div className='hidden lg:block ml-4 md:ml-8'>
-            <RightBar />
-          </div>
-        </div> */}
-          {/* with grid end  */}
-        </body>
+        <body>{children}</body>
       </ClerkProvider>
     </html>
   );
