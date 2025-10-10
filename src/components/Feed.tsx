42a1ba6d4a2a9ec8e66c3a0da87603821ed115cd
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-10 11:18:40 +0530

    following post issue fixed

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,16 +1,43 @@
 import React from "react";
 import Post from "./Post";
 import { prisma } from "@/prisma";
+import { auth } from "@clerk/nextjs/server";
 
-const Feed = async () => {
-  const posts = await prisma.post.findMany();
+const Feed = async ({ userProfileId }: { userProfileId: string }) => {
+  const { userId } = await auth();
+
+  if (!userId) return;
+  const whereCondition = userProfileId
+    ? { userId: userProfileId }
+    : {
+        userId: {
+          in: [
+            userId,
+            ...(
+              await prisma.follow.findMany({
+                where: { followerId: userId },
+                select: { followingId: true },
+              })
+            ).map((follow) => follow.followingId),
+          ],
+        },
+      };
+
+
+  const posts = await prisma.post.findMany({
+    where: whereCondition,
+  });
+
+  console.log("the output: ", posts);
+
+  // FETCH USER FROM CURRENT USER AND FOLLOWING
   return (
     <div>
-      {posts.map((post) => (
+      {/* {posts.map((post) => (
         <div className='' key={post.id}>
           <Post type='post' />
         </div>
-      ))}
+      ))} */}
     </div>
 
     // <div>
